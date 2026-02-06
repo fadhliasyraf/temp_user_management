d26720879d5e33cc5e4c959f6de376410a3b88ea
fadhliasyraf <fadhliasyraf93@gmail.com>
2026-02-06 13:03:36 +0800

    asd

--- proj/models/model.py
+++ proj/models/model.py
@@ -1,6 +1,9 @@
 import uuid
 from flask import json
+from pwdlib import PasswordHash
+
 from proj.models import db
+from sqlalchemy import event
 
 user_roles = db.Table(
     'user_roles',
@@ -81,4 +84,52 @@ class Rank(db.Model):
         self.name = name
 
 
+
+
+@event.listens_for(db.metadata, 'after_create')
+def create_default_user(*args, **kwargs):
+    # Default ranks
+    default_ranks = [
+        ('ADMIN', 'Admin'),
+        ('MAJOR', 'Major'),
+        ('CAPTAIN', 'Captain'),
+        ('SARGEANT', 'Sargeant')
+    ]
+
+    for code, name in default_ranks:
+        if not Rank.query.filter_by(code=code).first():
+            db.session.add(Rank(code, name))
+
+    # Default roles
+    default_roles = [
+        ('ADMIN', 'Admin'),
+        ('MAINTENANCE', 'Maintenance'),
+        ('WAROFFICE', 'War Office')
+    ]
+
+    for code, name in default_roles:
+        if not Role.query.filter_by(code=code).first():
+            db.session.add(Role(code, name))
+
+    db.session.commit()
+
+    # Default admin user
+    if not User.query.filter_by(username='admin').first():
+        pwd_hasher = PasswordHash.recommended()
+        hashed_password = pwd_hasher.hash('admin')
+
+        selectedRank = Rank.query.filter_by(code="ADMIN").first()
+        selectedRole = Role.query.filter_by(code="ADMIN").first()
+
+        default_user = User(
+            name='System Admin',
+            rank_uuid=selectedRank.uuid,
+            username='admin',
+            password=hashed_password
+        )
+        default_user.roles.append(selectedRole)
+
+        db.session.add(default_user)
+        db.session.commit()
+        print("Default admin user created")
 
