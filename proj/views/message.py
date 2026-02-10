312bda24f24706c72ba62cbb5cd2018a21726253
mahdimy <mmy092@hotmail.com>
2026-02-10 16:09:51 +0800

    Add get_message endpoint to retrieve messages for a specific user role, including error handling for exceptions

--- proj/views/message.py
+++ proj/views/message.py
@@ -19,3 +19,26 @@ def send_message():
 
     return jsonify(response)
 
+
+@bp_message.route('/get_message/<user_role>', methods=['GET'])
+def get_message(user_role):
+    response = dict(code='111', data=dict(), description="Message retrieved successfully", status="OK")
+    try:
+        messages = Message.query.filter_by(to_user=user_role, isDeleted=None).all()
+
+        messages_list = [{
+            'id': msg.id,
+            'from_user': msg.from_user,
+            'to_user': msg.to_user,
+            'message_type': msg.type,
+            'message': msg.message,
+            'date_created': msg.date_created.strftime('%d/%m/%Y %H:%M:%S') if msg.date_created else None
+        } for msg in messages]
+
+        response['data'] = messages_list
+
+    except Exception as e:
+        msg = str(e)
+        response = dict(code='000', data='', description=str(msg), status="FAILED")
+
+    return jsonify(response)
